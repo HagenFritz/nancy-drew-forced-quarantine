72fdd67b737aa9d739f84ddf9d545a764cbc194e
Hagen E. Fritz <hagenfritz@gmail.com>
2020-12-18 00:24:58 -0600

    new easter eggs

--- src/config_files/GameProgress.py
+++ src/config_files/GameProgress.py
@@ -5,10 +5,14 @@ class Progress():
 		# Medals
 		self.easter_egg_count = 0
 		self.nugget_clicks = 0
+		self.star_wars_count = 0
 
 		# Easter Eggs
 		self.ufo_clicked = False
 		self.duct_clicked = False
+		self.xwing_clicked = False
+		self.ywing_clicked = False
+		self.tie_clicked = False
 
 		# Story progress
 		self.met_ingman = False
